544b9e495e04ed3256bca31833b82e2c3536ea2c
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-12-28 16:27:42 +0000

    Cração de exercicios e simuladores...

--- resolucaoExerciciosFundamentos/Program.cs
+++ resolucaoExerciciosFundamentos/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Fundamentos de Programação com dotnet csharp
+
+int primeiroNumero = 7;
+int segundoNumero = 2;
+
+int soma = primeiroNumero + segundoNumero;
+int subtracao = primeiroNumero - segundoNumero;
+int multiplicacao = primeiroNumero * segundoNumero;
+int parteInteiraDaDivisao = primeiroNumero / segundoNumero;
+int restoDaDivisao = primeiroNumero % segundoNumero;
+
+
+
+
