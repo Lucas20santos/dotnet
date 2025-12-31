ec69d41c9be135f381a6150c4111851b48640094
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-12-31 01:05:24 +0000

    Subindo alterações

--- resolucaoExercicioDeCondicionais/Program.cs
+++ resolucaoExercicioDeCondicionais/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// IF
+
+int idade = 18;
+
+if (idade >= 18) Console.WriteLine("Maior de idade.");
+
+// IF - ELSE
+
+double nota = 6;
+
+if (nota => 7) Console.WriteLine("Aprovado"); else Console.WriteLine("Reprovado");
+
+// IF - ELSE IF - ELSE
+
+// if (nota => 9) Console.WriteLine("Excelente");
+// else if (nota => 7) Console.WriteLine("Aprovado");
+// else Console.WriteLine("Reprovado");
+
+
--- resolucaoExerciciosFundamentos/obj/Debug/net10.0/resolucaoExerciciosFundamentos.AssemblyInfo.cs
+++ resolucaoExerciciosFundamentos/obj/Debug/net10.0/resolucaoExerciciosFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("resolucaoExerciciosFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c198275c0b9b1cc1adbecd8445b2fdb3f10d89e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c521f3bbe0f7e026056eb097dfeb4a2accd03cd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("resolucaoExerciciosFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("resolucaoExerciciosFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
