6ea5586568606cb64be44c3c04ef8c0292d777f1
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-12-31 21:47:40 +0000

    Subindo mudanças encontradas...

--- resolucaoExercicioDeCondicionais/Program.cs
+++ resolucaoExercicioDeCondicionais/Program.cs
@@ -1,67 +1,90 @@
-﻿// IF
+﻿// // IF
 
-int idade = 18;
+// int idade = 18;
 
-if (idade >= 18) Console.WriteLine("Maior de idade.");
+// if (idade >= 18) Console.WriteLine("Maior de idade.");
 
-// IF - ELSE
+// // IF - ELSE
 
-double nota = 6;
+// double nota = 6;
 
-if (nota >= 7) Console.WriteLine("Aprovado"); else Console.WriteLine("Reprovado");
+// if (nota >= 7) Console.WriteLine("Aprovado"); else Console.WriteLine("Reprovado");
 
-// IF - ELSE IF - ELSE
+// // IF - ELSE IF - ELSE
 
-if (nota >= 9) Console.WriteLine("Excelente");
-else if (nota >= 7) Console.WriteLine("Aprovado");
-else Console.WriteLine("Reprovado");
+// if (nota >= 9) Console.WriteLine("Excelente");
+// else if (nota >= 7) Console.WriteLine("Aprovado");
+// else Console.WriteLine("Reprovado");
 
-// Operador Ternário ?
+// // Operador Ternário ?
 
-Console.WriteLine(nota >= 7 ? "Aprovado" : "Reprovado");
+// Console.WriteLine(nota >= 7 ? "Aprovado" : "Reprovado");
 
-// Switch
+// // Switch
 
-int dia = 3;
+// int dia = 3;
 
-switch(dia)
-{
-    case 1:
-        Console.WriteLine("Segunda");
-        break;
-    case 2:
-        Console.WriteLine("Terça");
-        break;
-    case 3:
-        Console.WriteLine("Quarta");
-        break;
-    default:
-        Console.WriteLine("Dia inválido");
-        break;
-}
+// switch(dia)
+// {
+//     case 1:
+//         Console.WriteLine("Segunda");
+//         break;
+//     case 2:
+//         Console.WriteLine("Terça");
+//         break;
+//     case 3:
+//         Console.WriteLine("Quarta");
+//         break;
+//     default:
+//         Console.WriteLine("Dia inválido");
+//         break;
+// }
 
-// SWITCH C# MODERNO
+// // SWITCH C# MODERNO
 
-dia = 6;
+// dia = 6;
 
-string nomeDia = dia switch
-{
-    0 => "Domingo",
-    1 => "Segunda",
-    2 => "Terça",
-    3 => "Quarta",
-    4 => "Quinta",
-    5 => "Sexta",
-    6 => "Sábado",
-    _ => "Inválido"
-};
+// string nomeDia = dia switch
+// {
+//     0 => "Domingo",
+//     1 => "Segunda",
+//     2 => "Terça",
+//     3 => "Quarta",
+//     4 => "Quinta",
+//     5 => "Sexta",
+//     6 => "Sábado",
+//     _ => "Inválido"
+// };
 
-Console.WriteLine(nomeDia);
+// Console.WriteLine(nomeDia);
 
-// CONDICIONAIS COM NULL
+// // CONDICIONAIS COM NULL
 
-double? nota1 = null;
-double notaFinal = nota1 ?? 0;
+// double? nota1 = null;
+// double notaFinal = nota1 ?? 0;
 
-Console.WriteLine(notaFinal);
+// Console.WriteLine(notaFinal);
+
+// EXERCICIO 01
+
+// Console.Write("Digite um número inteiro: ");
+// string? entrada = Console.ReadLine();
+
+// if (string.IsNullOrWhiteSpace(entrada))
+// {
+//     Console.WriteLine("Sua entrada não pode ser vazia ou nulla.");
+//     return;
+// }
+
+// if(!int.TryParse(entrada, out int numero))
+// {
+//     Console.WriteLine("Por favor informar um número interio.");
+//     return;
+// }
+
+// string ehPar = numero % 2 == 0 ? "Par" : "Impar";
+
+// Console.WriteLine($"O número: {numero} é {ehPar}.");
+
+
 
--- resolucaoExerciciosFundamentos/obj/Debug/net10.0/resolucaoExerciciosFundamentos.AssemblyInfo.cs
+++ resolucaoExerciciosFundamentos/obj/Debug/net10.0/resolucaoExerciciosFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("resolucaoExerciciosFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec69d41c9be135f381a6150c4111851b48640094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d60c1f1f638a98e995f4ae3b9d60429d8910efd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("resolucaoExerciciosFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("resolucaoExerciciosFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
